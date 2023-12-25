5f990dcf2662eb059305dff3d712cc62998d852e
kemsakurai <sakurai.kem@mail.com>
2023-12-26 08:23:48 +0900

    Update cli.py

--- cli.py
+++ cli.py
@@ -1,4 +1,5 @@
 import pprint
+from datetime import datetime, date
 
 import click
 import os
@@ -15,7 +16,11 @@ def cli():
 @click.argument('issue_id')
 @click.argument('web_hook_token')
 @click.option('--label', '-l', default='bug')
-def send_issue_info(issue_id: str, label: str, web_hook_token: str):
+@click.option('--pullrquest_identification_string', '-p', default='**■ 不具合混入pull requests URL** >')
+@click.option('--failure_date_identification_string', '-fd', default='**■ 障害発生日時** >')
+@click.option('--failure_resolution_date_identification_string', '-frd', default='**■ 障害解消日時** >')
+def send_issue_info(issue_id: str, label: str, web_hook_token: str, pullrquest_identification_string: str,
+                    failure_date_identification_string: str, failure_resolution_date_identification_string: str):
     url = "https://api.github.com/graphql"
     token = os.getenv('GITHUB_TOKEN')
     headers = {'Authorization': 'Bearer ' + token}
@@ -69,14 +74,15 @@ def send_issue_info(issue_id: str, label: str, web_hook_token: str):
         payload['updatedAt'] = response['updatedAt']
         payload['url'] = response['url']
         # 原因 pullrequest URL
-        pullrequest_caused_bug = _get_defected_pull_requests_url(body)
+        pullrequest_caused_bug = _get_defected_pull_requests_url(body, pullrquest_identification_string)
         payload['pullrequestCausedBug'] = pullrequest_caused_bug
-        failure_info = _get_failure_info(body)
+        failure_info = _get_failure_info(body, failure_date_identification_string,
+                                         failure_resolution_date_identification_string)
         payload['failureDatetime'] = failure_info.get("failure_datetime")
         payload['failureCompletionDatetime'] = failure_info.get("failure_completion_datetime")
         payload['time_to_restore_service'] = failure_info.get("time_to_restore_service")
         import json
-        json_str = json.dumps(payload, sort_keys=True, indent=2)
+        json_str = json.dumps(payload, sort_keys=True, indent=2, default=_json_serial)
         f = open('payload.json', 'w')
         f.write(json_str)
         f.close()
@@ -85,32 +91,42 @@ def send_issue_info(issue_id: str, label: str, web_hook_token: str):
         print('Since the label to be sent by webhook is not included, the sending process will be skipped.')
 
 
-def _get_failure_info(description):
+def _get_failure_info(description, failure_date_identification_string, failure_resolution_date_identification_string):
     failure_datetime = None
     failure_completion_datetime = None
     time_to_restore_service = None
 
     for line in description.splitlines():
-        if '**■ 障害発生日時** >' in line:
-            failure_datetime = parser.parse(line.split('>')[1].strip())
+        if failure_date_identification_string in line:
+            failure_datetime = parser.parse(line.split(failure_date_identification_string)[1].strip())
 
-        elif '**■ 障害解消日時**' in line:
-            failure_completion_datetime = parser.parse(line.split('>')[1].strip())
+        elif failure_resolution_date_identification_string in line:
+            failure_completion_datetime = \
+                parser.parse(line.split(failure_resolution_date_identification_string)[1].strip())
 
     if failure_datetime is not None and failure_completion_datetime is not None:
         time_to_restore_service = (failure_completion_datetime - failure_datetime).total_seconds()
 
     return {
         'failure_datetime': failure_datetime,
-        'failure_completion_datetime':failure_completion_datetime,
+        'failure_completion_datetime': failure_completion_datetime,
         'time_to_restore_service': time_to_restore_service
     }
 
 
-def _get_defected_pull_requests_url(description):
+# date, datetimeの変換関数
+def _json_serial(obj):
+    # 日付型の場合には、文字列に変換します
+    if isinstance(obj, (datetime, date)):
+        return obj.isoformat(timespec='milliseconds').replace('.000', 'Z')
+    # 上記以外はサポート対象外.
+    raise TypeError("Type %s not serializable" % type(obj))
+
+
+def _get_defected_pull_requests_url(description, pullrquest_identification_string):
     for line in description.splitlines():
-        if '■ 不具合混入pull requests URL' in line:
-            return line.split('>')[1].strip()
+        if pullrquest_identification_string in line:
+            return line.split(pullrquest_identification_string)[1].strip()
     return ''
 
 
